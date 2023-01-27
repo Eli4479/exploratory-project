f30b5a13d2fddf6c91b1f1eb5832b0bbd25295a8
Eli4479 <patel.aryan.eee21@itbhu.ac.in>
2023-01-28 00:38:23 +0530

    added backend

---
+++ backend/models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  roll_number: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    validate: {
+      validator: function (v) {
+        return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
+      }
+    }
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
